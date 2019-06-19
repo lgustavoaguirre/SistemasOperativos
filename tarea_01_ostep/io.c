a551cb8d12fa6dda2e81c27de57b596348c08285
lgustavoaguirre <gustavo.aguirre@correounivalle.edu.co>
2019-06-19 15:57:51 -0500

    archivo para invertir

--- tarea_01_ostep/io.c
+++ tarea_01_ostep/io.c
@@ -1,20 +1,101 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <assert.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+
+#include <stdlib.h>
+
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    assert(fd >= 0);
-    char buffer[20];
-    sprintf(buffer, "hello world\n");
-    int rc = write(fd, buffer, strlen(buffer));
-    assert(rc == (strlen(buffer)));
-    fsync(fd);
-    close(fd);
-    return 0;
+
+
+void copia_invertida(char *nombrearchivoleer, char *nombrearchivoescribir);
+
+
+
+int main(int argc, char* argv[]){
+
+	copia_invertida("entrada.txt","salida.txt");
+
+	return 0;
+
 }
 
+
+
+
+
+//Función que copia el contenido de un archivo en otro invirtiendo cada línea
+
+void copia_invertida(char *nombrearchivoleer, char *nombrearchivoescribir){
+
+	FILE *parchivolectura, *parchivoescritura;
+
+	//char linealeida[NUMCARACTERES], lineainvertida[NUMCARACTERES];
+
+	int fin=0;
+
+ 
+
+	//Abrimos los archivos
+
+	parchivolectura = fopen(nombrearchivoleer,"r");
+
+	parchivoescritura = fopen(nombrearchivoescribir,"w");
+
+	
+
+	int i=0;   // movera el puntero a los caracteres del archivo
+
+	
+
+	
+
+	//Comprobamos si la apertura ha sido correcta
+
+	if ((parchivolectura == NULL)||(parchivoescritura == NULL)){
+
+		printf("Error al abrir ficheros");
+
+	}else {
+
+	//Recorremos el archivo de lectura mientras no se acabe
+
+		do {
+
+		
+
+		fseek(parchivolectura, -2 - i, SEEK_END);
+
+	    char f = getc(parchivolectura);
+
+	    char a = ftell(parchivolectura);
+
+		i++;
+
+	    if(a==1){
+
+			// Este es el ultimo caracter que se va a leer
+
+			fprintf(parchivoescritura, "%c", f);
+			
+			fin=1;
+
+		}else
+
+		    fprintf(parchivoescritura, "%c", f);
+
+		}while (fin!=1);
+
+		
+
+		//Cuando llegamos al final del archivo de lectura, cerramos los archivos
+
+		fclose(parchivolectura);
+
+		fclose(parchivoescritura);
+
+	}
+
+ return;
+
+
+
+}
